f36470cb2a46a85f7cd780b86894f5f3e7e57d3d
Fabio Filippini <fipsfabio95@gmail.com>
2022-03-04 19:16:03 +0100

    pseudo

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-console.log('js ok');
+// console.log('js ok');
 
-const kmToGo = window.prompt('Quanti chilometri devi percorrere?');
+const kmToGo = parseInt(prompt('Quanti chilometri devi percorrere?'));
 
-const age = window.prompt('Quanti anni hai?');
+const age = parseInt(prompt('Quanti anni hai?'));
 
 const fullPrice = (kmToGo * 0.21)
 
@@ -10,12 +10,29 @@ const underAge = 18
 
 const discountUnderAge = (fullPrice * 20) / 100;
 
-const adultAge = 65
+const adultAge = 66
 
 const discountAdultAge = (fullPrice * 40) / 100;
 
-
 console.log(fullPrice + ' euro')
+
+if (isNaN(kmToGo)) {
+    alert('Si accettano solo numeri!');
+    document.getElementById('price').innerText = 'Riaggiornare ed inserire solo numeri!';
+    console.log('Riaggiornare ed inserire solo numeri!')
+}
+
+
+if (isNaN(age)) {
+    document.getElementById('price').innerText = 'Riaggiornare ed inserire solo numeri!';
+    console.log('Riaggiornare ed inserire solo numeri!')
+}
+
+
+// if (isNaN(kmToGo)) {
+
+//     
+// }
 
 
 
@@ -23,15 +40,12 @@ console.log(fullPrice + ' euro')
 if (age < underAge) {
 
     const finalPrice = (fullPrice - discountUnderAge);
-    // finalPrice.toFixed(2);
-    // const finalPrice = Math.round ( * 100) / 100;
-
-
     console.log(discountUnderAge.toFixed(2) + ' euro')
     console.log(finalPrice.toFixed(2) + ' euro')
-
-
+    document.getElementById('price').innerText = finalPrice.toFixed(2);
 }
+
+
 
 
 if (age > adultAge) {
@@ -39,8 +53,10 @@ if (age > adultAge) {
     const finalPrice = (fullPrice - discountAdultAge);
     console.log(discountAdultAge.toFixed(2) + ' euro')
     console.log(finalPrice.toFixed(2) + ' euro')
-
+    document.getElementById('price').innerText = finalPrice.toFixed(2);
 
 }
+
+
 
 
