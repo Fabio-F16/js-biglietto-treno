36b9b3186944c334a3afd44205508a54091bd54d
Fabio Filippini <fipsfabio95@gmail.com>
2022-03-04 14:52:23 +0100

    parziale

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log('js ok');
+
+const kmToGo = window.prompt('Quanti chilometri devi percorrere?');
+
+const age = window.prompt('Quanti anni hai?');
+
+const fullPrice = (kmToGo * 0.21)
+
+const underAge = 18
+
+const discountUnderAge = (fullPrice * 20) / 100;
+
+const adultAge = 65
+
+const discountAdultAge = (fullPrice * 40) / 100;
+
+
+console.log(fullPrice)
+
+
+
+
+if (age < underAge) {
+
+    const finalPrice = (fullPrice - discountUnderAge);
+    const finalPrice = (Math.round(finalPrice * 100) / 100);
+
+
+    console.log(discountUnderAge)
+    console.log(finalPrice)
+
+
+}
+
+
+if (age > adultAge) {
+
+    const finalPrice = (fullPrice - discountAdultAge);
+    console.log(discountAdultAge)
+    console.log(finalPrice)
+
+
+}
+
+
